9afaa205ab838f0f1b6c4df04f2d9bb6468e55e8
Hamzabaloch08 <hb.developer08@gmail.com>
2024-09-12 17:22:03 +0500

    Create cssQuiz.js

---
+++ cssQuiz/cssQuiz.js
@@ -0,0 +1,204 @@
+let cssQuestions = [
+    {
+        question: 'What does CSS stand for?',
+        option1: 'Colorful Style Sheet',
+        option2: 'Computer Style Sheet',
+        option3: 'Cascading Style Sheet',
+        correctOption: 'Cascading Style Sheet'
+    },
+    {
+        question: 'What is the correct HTML for referring to an external style sheet?',
+        option1: '<stylesheet>mystyle.css</stylesheet>',
+        option2: '<style src="mystyle.css">',
+        option3: '<link rel="stylesheet" type="text/css" href="mystyle.css">',
+        correctOption: '<link rel="stylesheet" type="text/css" href="mystyle.css">'
+    },
+    {
+        question: 'Where in an HTML document is the correct place to refer to an external style sheet?',
+        option1: 'In the <body> section',
+        option2: 'In the <head> section',
+        option3: 'At the end of the document',
+        correctOption: 'In the <head> section'
+    },
+    {
+        question: 'Which HTML tag is used to define an internal style sheet?',
+        option1: '<css>',
+        option2: '<style>',
+        option3: '<script>',
+        correctOption: '<style>'
+    },
+    {
+        question: 'Which HTML attribute is used to define inline styles?',
+        option1: 'styles',
+        option2: 'font',
+        option3: 'style',
+        correctOption: 'style'
+    },
+    {
+        question: 'Which is the correct CSS syntax?',
+        option1: 'body{color: black;}',
+        option2: '{body:color=black;}',
+        option3: 'body:color=black;',
+        correctOption: 'body{color: black;}'
+    },
+    {
+        question: 'How do you insert a comment in a CSS file?',
+        option1: '/*this is a comment*/',
+        option2: '//this is a comment',
+        option3: '//this is a comment//',
+        correctOption: '/*this is a comment*/'
+    },
+    {
+        question: 'Which property is used to change the background color?',
+        option1: 'bgcolor',
+        option2: 'color',
+        option3: 'background-color',
+        correctOption: 'background-color'
+    },
+    {
+        question: 'How do you add a background color for all <h1> elements?',
+        option1: 'h1 {background-color:#FFFFFF;}',
+        option2: 'h1.all {background-color:#FFFFFF;}',
+        option3: 'all.h1 {background-color:#FFFFFF;}',
+        correctOption: 'h1 {background-color:#FFFFFF;}'
+    },
+    {
+        question: 'Which CSS property is used to change the text color of an element?',
+        option1: 'color',
+        option2: 'text-color',
+        option3: 'fgcolor',
+        correctOption: 'color'
+    },
+    {
+        question: 'Which CSS property controls the text size?',
+        option1: 'font-size',
+        option2: 'text-style',
+        option3: 'text-size',
+        correctOption: 'font-size'
+    },
+    {
+        question: 'What is the correct CSS syntax for making all the <p> elements bold?',
+        option1: 'p {font-weight:bold;}',
+        option2: '<p style="font-size:bold;">',
+        option3: '<p style="text-size:bold;">',
+        correctOption: 'p {font-weight:bold;}'
+    },
+    {
+        question: 'How do you display hyperlinks without an underline?',
+        option1: 'a {underline:none;}',
+        option2: 'a {text-decoration:no-underline;}',
+        option3: 'a {text-decoration:none;}',
+        correctOption: 'a {text-decoration:none;}'
+    },
+    {
+        question: 'How do you make each word in a text start with a capital letter?',
+        option1: 'text-style:capitalize',
+        option2: 'text-transform:capitalize',
+        option3: 'You can not do that with CSS',
+        correctOption: 'text-style:capitalize'
+    },
+    {
+        question: 'Which property is used to change the font of an element?',
+        option1: 'font-style',
+        option2: 'font-weight',
+        option3: 'font-family',
+        correctOption: 'font-family'
+    },
+    {
+        question: 'How do you make the text bold?',
+        option1: 'style:bold;',
+        option2: 'font:bold;',
+        option3: 'font-weight:bold;',
+        correctOption: 'font-weight:bold;'
+    },
+    {
+        question: 'Which property is used to change the left margin of an element?',
+        option1: 'margin-left',
+        option2: 'padding-left',
+        option3: 'indent',
+        correctOption: 'margin-left'
+    },
+    {
+        question: 'When using the padding property; are you allowed to use negative values?',
+        option1: 'Yes',
+        option2: 'No',
+        option3: 'Only in some browsers',
+        correctOption: 'No'
+    },
+    {
+        question: 'How do you make a list that lists its items with squares?',
+        option1: 'list: square;',
+        option2: 'list-type: square;',
+        option3: 'list-style-type: square;',
+        correctOption: 'list-style-type: square;'
+    },
+    {
+        question: 'How do you select an element with id "demo"?',
+        option1: '#demo',
+        option2: '.demo',
+        option3: 'demo',
+        correctOption: '#demo'
+    }
+]
+
+let ques = document.getElementById('ques')
+let opt1 = document.getElementById('opt1')
+let opt2 = document.getElementById('opt2')
+let opt3 = document.getElementById('opt3')
+let index = 0
+let enableButton = document.getElementById('btn')
+let score = 0
+let timer = document.getElementById('time')
+let time = 6
+
+let interval = setInterval(function () {
+
+    time--
+    timer.innerText = time
+    if (time == 0) {
+        time = 6
+        let getOptions = document.getElementsByName('options')
+        for (let i = 0; i < getOptions.length; i++) {
+            if (getOptions[i].checked) {
+                getOptions[i].checked = false
+            }
+        }
+        nextQues()
+    }
+
+}, 1000)
+
+function nextQues() {
+    let getOptions = document.getElementsByName('options')
+    for (let i = 0; i < getOptions.length; i++) {
+        if (getOptions[i].checked) {
+            let selectedValue = getOptions[i].value
+            let selectedAnswer = cssQuestions[index - 1][`option${selectedValue}`]
+            let correctOption = cssQuestions[index - 1][`correctOption`]
+            if (selectedAnswer == correctOption) {
+                score++
+            }
+            console.log(selectedAnswer)
+        }
+        getOptions[i].checked = false
+    }
+    enableButton.disabled = true
+    if (index >= cssQuestions.length) {
+        alert(`${(score / cssQuestions.length) * 100}%`);
+        clearInterval(interval)
+    } else {
+        ques.innerText = cssQuestions[index].question
+        opt1.innerText = cssQuestions[index].option1
+        opt2.innerText = cssQuestions[index].option2
+        opt3.innerText = cssQuestions[index].option3
+        index++
+        time = 6
+    }
+}
+
+nextQues()
+
+
+function clicked() {
+    enableButton.disabled = false
+}
